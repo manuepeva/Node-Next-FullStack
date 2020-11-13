5564e5d43d9ab91ef00b981a1a2f8475917d3f22
ManuDeveloper <manuepeva@gmail.com>
2020-11-13 06:54:28 -0500

    using localstorage in order to set the user session

---
+++ node-send-cliente/context/auth/authReducer.js
@@ -0,0 +1,33 @@
+import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    LIMPIAR_ALERTA,
+    LOGIN_ERROR,
+    LOGIN_EXITOSO
+} from '../../context/types/'
+
+export default (state, action) => {
+    switch (action.type) {
+        case REGISTRO_EXITOSO:
+        case REGISTRO_ERROR:
+        case LOGIN_ERROR:
+            return {
+                ...state,
+                mensaje: action.payload
+            }
+        case LOGIN_EXITOSO:
+            localStorage.setItem('token', action.payload)
+            return {
+                ...state,
+                token: action.payload,
+                autenticado: true
+            }
+        case LIMPIAR_ALERTA:
+            return {
+                ...state,
+                mensaje: null
+            }
+        default:
+            return state
+    }
+}
