c9981fe6eded3ccd2a8012b313dd4953a3c6a4a7
ManuDeveloper <manuepeva@gmail.com>
2020-11-16 06:13:09 -0500

    creating dynamic routing for the new generated user shared links

--- node-send-cliente/context/app/appState.js
+++ node-send-cliente/context/app/appState.js
@@ -1,11 +1,13 @@
 import React, { useReducer } from 'react'
+import clienteAxios from '../../config/axios'
 import {
     SUBIR_ARCHIVO_ERROR,
     SUBIR_ARCHIVO_EXITO,
     MOSTRAR_ALERTA,
     OCULTAR_ALERTA,
     CREAR_ENLACE_ERROR,
-    CREAR_ENLACE_EXITO
+    CREAR_ENLACE_EXITO,
+    SUBIR_ARCHIVO
 } from '../../context/types/index'
 import appContext from './appContext'
 import appReducer from './appReducer'
@@ -13,7 +15,14 @@ import appReducer from './appReducer'
 
 const AppState = ({ children }) => {
     const initialState = {
-        mensaje_archivo: ''
+        mensaje_archivo: '',
+        nombre: '',
+        nombre_original: '',
+        cargando: null,
+        descargas: 1,
+        password: '',
+        autor: null,
+        url: ''
     }
 
     // Crear dispatch y state 
@@ -31,11 +40,60 @@ const AppState = ({ children }) => {
             })
         }, 3000);
     }
+    // Sube los archivos al servidor
+    const subirArchivo = async (formData, nombreArchivo) => {
+        dispatch({
+            type: SUBIR_ARCHIVO
+        })
+        try {
+            const resultado = await clienteAxios.post('/api/archivos', formData)
+            dispatch({
+                type: SUBIR_ARCHIVO_EXITO,
+                payload: {
+                    nombre: resultado.data.archivo,
+                    nombre_original: nombreArchivo
+                }
+            })
+        } catch (error) {
+            dispatch({
+                type: SUBIR_ARCHIVO_ERROR,
+                payload: error.response.data.msg
+            })
+        }
+    }
+    // Crea un enlace una vez que se subió un archivo
+    const crearEnlace = async () => {
+        const data = {
+            nombre: state.nombre,
+            nombre_original: state.nombre_original,
+            descargas: state.descargas,
+            password: state.password,
+            autor: state.autor
+        }
+        try {
+            const resultado = await clienteAxios.post('/api/enlaces', data)
+            dispatch({
+                type: CREAR_ENLACE_EXITO,
+                payload: resultado.data.msg
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <appContext.Provider
             value={{
+                nombre: state.nombre,
+                nombre_original: state.nombre_original,
                 mensaje_archivo: state.mensaje_archivo,
-                mostrarAlerta
+                cargando: state.cargando,
+                descargas: state.descargas,
+                password: state.password,
+                autor: state.autor,
+                url: state.url,
+                mostrarAlerta,
+                subirArchivo,
+                crearEnlace
             }}
         >
             {children}
--- node-send-cliente/pages/index.js
+++ node-send-cliente/pages/index.js
@@ -15,32 +15,47 @@ const Index = () => {
   const { usuarioAutenticado } = AuthContext
   // Extraer el mensaje de error de archivos
   const AppContext = useContext(appContext)
-  const { mensaje_archivo } = AppContext
+  const { mensaje_archivo, url } = AppContext
   useEffect(() => {
     usuarioAutenticado()
   }, [])
   return (
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-        {mensaje_archivo && <Alerta />}
-        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-          <Dropzone />
-          <div className="md:flex-1 mb-3 mx-2 -mt-16 lg:mt-0">
-            <h2 className="text-2xl font-sans font-bold text-gray-800 my-4">
-              Compartir Archivos de Manera Sencilla y Privada</h2>
-            <p className="text-lg leading-loose">
-              <span className="text-red-500 font-bold">ReactNodeSend</span>
-                Te permite compartir archivos con cifrado de extremo a extremo y un archivo que
-                es eliminado después de ser descargado. Asi que puedes mantener lo que compartes
-                en privado y asegurarte de que tus cosas no permanezcan en la nube para siempre.
-              </p>
-            <Link href="/crearcuenta">
-              <a className="text-red-500 font-bold text-lg hover:text-red-700">
-                Crea una Cuenta para Mayores Beneficios
-                </a>
-            </Link>
-          </div>
-        </div>
+        {url ? (
+          <>
+            <p className="text-center text-2xl mt-10">
+              <span className="uppercase font-bold text-red-700 text-3xl">
+                Tu URL es: </span>{`${process.env.frontendURL}/enlaces/${url}`}</p>
+            <button
+              type="button"
+              className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold mt-10"
+              onClick={() => navigator.clipboard.writeText(`${process.env.frontendURL}/enlaces/${url}`)}
+            >Copiar Enlace</button>
+          </>
+        ) : (
+            <>
+              {mensaje_archivo && <Alerta />}
+              <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+                <Dropzone />
+                <div className="md:flex-1 mb-3 mx-2 -mt-16 lg:mt-0">
+                  <h2 className="text-2xl font-sans font-bold text-gray-800 my-4">
+                    Compartir Archivos de Manera Sencilla y Privada</h2>
+                  <p className="text-lg leading-loose">
+                    <span className="text-red-500 font-bold">ReactNodeSend</span>
+                  Te permite compartir archivos con cifrado de extremo a extremo y un archivo que
+                  es eliminado después de ser descargado. Asi que puedes mantener lo que compartes
+                  en privado y asegurarte de que tus cosas no permanezcan en la nube para siempre.
+                </p>
+                  <Link href="/crearcuenta">
+                    <a className="text-red-500 font-bold text-lg hover:text-red-700">
+                      Crea una Cuenta para Mayores Beneficios
+                  </a>
+                  </Link>
+                </div>
+              </div>
+            </>
+          )}
       </div>
     </Layout>
   )
